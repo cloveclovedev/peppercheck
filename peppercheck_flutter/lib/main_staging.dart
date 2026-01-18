cf612947c15e02d16eee744848d11906de3a6f48
Makoto Kurihara <mkuri8m@gmail.com>
2026-01-18 19:49:38 +0900

    feat(flutter): upsert fcm tokens on app startup (#189)
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- peppercheck_flutter/lib/main_staging.dart
+++ peppercheck_flutter/lib/main_staging.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:peppercheck_flutter/app/app.dart';
 import 'package:peppercheck_flutter/app/app_startup.dart';
 import 'package:peppercheck_flutter/app/config/app_environment.dart';
 
-void main() async {
-  await appStartup(AppConfig.staging);
-  runApp(const ProviderScope(child: MyApp()));
+void main() {
+  appStartup(AppConfig.staging);
 }
