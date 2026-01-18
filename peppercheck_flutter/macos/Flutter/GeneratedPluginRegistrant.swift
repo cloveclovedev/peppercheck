a26257a59341e70ab2fab0838179adbd3953df6f
Makoto Kurihara <mkuri8m@gmail.com>
2026-01-18 19:41:23 +0900

    feat(flutter): add firebase core and messaging libraries (#188)
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- peppercheck_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ peppercheck_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import firebase_core
+import firebase_messaging
 import flutter_timezone
 import google_sign_in_ios
 import in_app_purchase_storekit
@@ -15,6 +17,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
